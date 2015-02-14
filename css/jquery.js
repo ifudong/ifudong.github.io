77031d9177f49c40eb7d589400a87274f86d654a
sawyer <mathfudong@gmail.com>
2015-02-14 19:49:07 +0800

    new

---
+++ css/jquery.js
@@ -0,0 +1,153 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width,user-scalable=no">
+  	<title>页面没找到 - Smath</title>
+		<!--[if lt IE 9]>
+    <script src="http://cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="http://cdn.staticfile.org/normalize/3.0.1/normalize.min.css">
+    <link rel="stylesheet" href="http://smilenow.sinaapp.com/usr/themes/navy/style.css">
+    <script type="text/javascript" src="http://cdn.staticfile.org/jquery/1.11.0/jquery.min.js"></script>
+  <meta name="description" content="Just So So ..." />
+<meta name="keywords" content="typecho,php,blog" />
+<link rel="pingback" href="http://smilenow.sinaapp.com/index.php/action/xmlrpc" />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://smilenow.sinaapp.com/index.php/action/xmlrpc?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://smilenow.sinaapp.com/index.php/action/xmlrpc?wlw" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="http://smilenow.sinaapp.com/index.php/feed/" />
+<link rel="alternate" type="application/rdf+xml" title="RSS 1.0" href="http://smilenow.sinaapp.com/index.php/feed/rss/" />
+<link rel="alternate" type="application/atom+xml" title="ATOM 1.0" href="http://smilenow.sinaapp.com/index.php/feed/atom/" />
+<script type="text/javascript" src="http://smilenow.sinaapp.com/usr/plugins/JWPlayer/player/jwplayer.js"></script><link rel="stylesheet" type="text/css" href="http://smilenow.sinaapp.com/usr/plugins/Hermit/assets/style/hermit.min.css" media="all" />
+
+<link rel="stylesheet" type="text/css" href="http://smilenow.sinaapp.com/usr/plugins/HighSlide/css/highslide.css" />
+<!--[if lt IE 7]>
+<link rel="stylesheet" type="text/css" href="http://smilenow.sinaapp.com/usr/plugins/HighSlide/css/highslide-ie6.css" />
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="http://smilenow.sinaapp.com/usr/plugins/HighlightJs/res/styles/solarized_dark.css" /></head>
+<body>
+<div class="body404">
+<div class="info404">
+<header id="header404" class="clearfix">
+  <div class="site-name404">
+    <i>404</i>
+  </div>
+</header>
+<section>
+  <div class="title404"><p>每一个平凡的日常<br/>都是连续发生中的奇迹</p></div>
+  <a class="index404" rel="nofollow" href="http://smilenow.sinaapp.com/">返回首页</a>
+</section>
+<footer id="footer404">
+	&copy; 2015 Smath.</span>
+</footer>
+</div>
+</div>
+<script>var hermit = {url: "http://smilenow.sinaapp.com/usr/plugins/Hermit/assets/swf", ajax_url: "http://smilenow.sinaapp.com/usr/plugins/Hermit/ajax.php"};
+</script><script src='http://smilenow.sinaapp.com/usr/plugins/Hermit/assets/script/hermit.min.js'></script>
+
+<script type="text/javascript" src="http://smilenow.sinaapp.com/usr/plugins/HighSlide/js/highslide-full.packed.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+hs.graphicsDir = "http://smilenow.sinaapp.com/usr/plugins/HighSlide/css/graphics/";
+hs.fadeInOut = true;
+hs.transitions = ["expand","crossfade"];
+hs.lang.creditsText = "&copy; 1.smilenow.sinaapp.com";
+hs.lang.creditsTitle = "&copy; 1.smilenow.sinaapp.com";
+hs.creditsHref = "http://smilenow.sinaapp.com/index.php";
+hs.creditsPosition = "top center";
+hs.lang={
+loadingText : "载入中...",
+loadingTitle : "取消",
+closeText : "关闭",
+closeTitle : "关闭 (Esc)",
+previousText : "上一张",
+previousTitle : "上一张 (←键)",
+nextText : "下一张",
+nextTitle : "下一张 (→键)",
+moveTitle : "移动",
+moveText : "移动",
+playText : "播放",
+playTitle : "幻灯播放 (空格键)",
+pauseText : "暂停",
+pauseTitle : "幻灯暂停 (空格键)",
+number : "第%1张 共%2张",
+restoreTitle :	"点击关闭或拖动. 左右方向键切换图片. ",
+fullExpandTitle : "完整尺寸",
+fullExpandText :  "原大"
+};
+hs.outlineType = "glossy-dark";
+hs.wrapperClassName = "dark";
+hs.captionEval = "this.a.title";
+hs.numberPosition = "caption";
+hs.dimmingOpacity = 0.65;
+hs.align = "center";
+if (hs.addSlideshow) hs.addSlideshow({
+slideshowGroup: "images",
+interval: 5000,
+repeat: true,
+useControls: true,
+fixedControls: "fit",
+overlayOptions: {
+opacity: .65,
+position: "bottom center",
+hideOnMouseOut: true
+}
+});
+//]]>
+</script>
+		<script type="text/javascript" src="http://smilenow.sinaapp.com/usr/plugins/HighlightJs/res/highlight.pack.js"></script><script type="text/javascript">window.onload = function () {
+var codes = document.getElementsByTagName("pre"),
+    hlNames = {
+        actionscript : /^as[1-3]$/i,
+        cmake : /^(make|makefile)$/i,
+        cs : /^csharp$/i,
+        css : /^css[1-3]$/i,
+        delphi : /^pascal$/i,
+        javascript : /^js$/i,
+        markdown : /^md$/i,
+        objectivec : /^objective\-c$/i,
+        php  : /^php[1-6]$/i,
+        sql : /^mysql$/i,
+        xml : /^(html|html5|xhtml)$/i
+    }, hlLangs = hljs.LANGUAGES;
+
+for (var i = 0; i < codes.length; i ++) {
+    var children = codes[i].getElementsByTagName("code"), highlighted = false;
+
+    if (children.length > 0) {
+        var code = children[0], className = code.className;
+
+        if (!!className) {
+            if (0 == className.indexOf("lang-")) {
+                var lang = className.substring(5).toLowerCase(), finalLang;
+            
+                if (hlLangs[lang]) {
+                    finalLang = lang;
+                } else {
+                    for (var l in hlNames) {
+                        if (lang.match(hlNames[l])) {
+                            finalLang = l;
+                        }
+                    }
+                }
+
+                if (!!finalLang) {
+                    var result = hljs.highlight(finalLang, code.textContent, true);
+                    code.innerHTML = result.value;
+                    highlighted = true;
+                }
+            }
+        }
+
+        if (!highlighted) {
+            var html = code.innerHTML;
+            code.innerHTML = html.replace(/<\/?[a-z]+[^>]*>/ig, "");
+            hljs.highlightBlock(code, "", false);
+        }
+    }
+}
+}</script></body>
+</html>
